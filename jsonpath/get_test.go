d2cddb7620fc7dc308a9e3993851f913d4a35a54
Pavel Shvedko <pshvedko@gmail.com>
2021-04-27 04:39:38 +0300

    Fix get_test.go

--- jsonpath/get_test.go
+++ jsonpath/get_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestGet(t *testing.T) {
-	var j = map[string]interface{}{
+	g := jsonpath.NewGetter(map[string]interface{}{
 		"access_level": "high",
 		"id": map[string]interface{}{
 			"value": 15,
@@ -59,62 +59,37 @@ func TestGet(t *testing.T) {
 				"status_id": 1,
 			},
 		},
-	}
-	type args struct {
-		j interface{}
-		k []string
-	}
+	})
 	tests := []struct {
 		name    string
-		args    args
 		want    interface{}
 		wantErr bool
 	}{
 		{
-			name: "object_array.list.#.status",
-			args: args{
-				j: j,
-				k: []string{"object_array", "list", "#", "status"},
-			},
+			name:    "object_array.list.#.status",
 			want:    []interface{}{"ok", "ok", "ok"},
 			wantErr: false,
 		}, {
-			name: "statuses.#.status_id",
-			args: args{
-				j: j,
-				k: []string{"statuses", "#", "status_id"},
-			},
+			name:    "statuses.#.status_id",
 			want:    []interface{}{4, 3, 1},
 			wantErr: false,
 		}, {
-			name: "num.value.all",
-			args: args{
-				j: j,
-				k: []string{"num", "value", "all"},
-			},
+			name:    "num.value.all",
 			want:    15,
 			wantErr: false,
 		}, {
-			name: "id.value",
-			args: args{
-				j: j,
-				k: []string{"id", "value"},
-			},
+			name:    "id.value",
 			want:    15,
 			wantErr: false,
 		}, {
-			name: "access_level",
-			args: args{
-				j: j,
-				k: []string{"access_level"},
-			},
+			name:    "access_level",
 			want:    "high",
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := jsonpath.Get(tt.args.j, tt.args.k)
+			got, err := g.Get(tt.name)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
 				return
